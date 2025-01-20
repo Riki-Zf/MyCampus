da5c3f301bdf636a9f612c748cd413fc684924c5
Riki-Zf <rikipatande81979@gmail.com>
2025-01-20 23:14:22 +0800

    add media query

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p> &copy; Edusity. All right reserved</p>
+      <ul>
+        <li>Terms of service</li>
+        <li>Privacy Police</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
